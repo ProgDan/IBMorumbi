86f590a15d139275140b7f013e83c32dd2048a1c
Daniel Arndt Alves <progdan@gmail.com>
2014-03-29 16:07:41 -0300

    Inclusão dos botões de links no Twitter e Facebook

---
+++ iOS/IBMorumbi/IBMorumbi/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  IBMorumbi
+//
+//  Created by Daniel Arndt Alves on 3/29/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ iOS/IBMorumbi/IBMorumbi/AboutViewController.m
@@ -0,0 +1,43 @@
+//
+//  AboutViewController.m
+//  IBMorumbi
+//
+//  Created by Daniel Arndt Alves on 3/29/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+- (IBAction)openFacebook:(UIButton *)sender;
+- (IBAction)openTwitter:(UIButton *)sender;
+
+@end
+
+@implementation AboutViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)openFacebook:(UIButton *)sender {
+    // Para pegar o ID da página no Facebook: https://graph.facebook.com/yourappspage
+    NSURL *facebookURL = [NSURL URLWithString:@"fb://profile/100002117943796"];
+    if ([[UIApplication sharedApplication] canOpenURL:facebookURL]) {
+        [[UIApplication sharedApplication] openURL:facebookURL];
+    } else {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.facebook.com/igreja.batistadomorumbi"]];
+    }
+}
+
+- (IBAction)openTwitter:(UIButton *)sender {
+    NSURL *twitterURL = [NSURL URLWithString:@"twitter://user?screen_name=ibmorumbi"];
+    if ([[UIApplication sharedApplication] canOpenURL:twitterURL]) {
+        [[UIApplication sharedApplication] openURL:twitterURL];
+    } else {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://twitter.com/ibmorumbi"]];
+    }
+}
+@end
