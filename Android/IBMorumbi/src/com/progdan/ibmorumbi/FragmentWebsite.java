31ab64b4a6157ccbd2b9ce50d392880c2b22413b
Daniel Arndt Alves <progdan@gmail.com>
2014-04-18 17:16:00 -0300

    Criação da tela de acesso ao website (Android)

--- Android/IBMorumbi/src/com/progdan/ibmorumbi/FragmentWebsite.java
+++ Android/IBMorumbi/src/com/progdan/ibmorumbi/FragmentWebsite.java
@@ -1,17 +1,33 @@
 package com.progdan.ibmorumbi;
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
+@SuppressLint("SetJavaScriptEnabled")
 public class FragmentWebsite extends Fragment {
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
-		View view = inflater.inflate(R.layout.tab, container, false);
-		TextView textView = (TextView) view.findViewById(R.id.tabtextview);
-		textView.setText(R.string.website);
+		View view = inflater.inflate(R.layout.website, container, false);
+		
+		String url = "http://www.ibmorumbi.com.br/";
+		WebView myWebView = (WebView) view.findViewById(R.id.webview);
+		WebSettings settings = myWebView.getSettings();
+		
+		settings.setUseWideViewPort(true);
+		settings.setBuiltInZoomControls(true);
+		settings.setSupportZoom(true);
+
+		settings.setJavaScriptEnabled(true);
+		settings.setLoadWithOverviewMode(true);
+
+		myWebView.loadUrl(url);
+		
 		return view;
 	}
 }
